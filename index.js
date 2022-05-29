1c00bdaa8c4674b65c19bfc75fbb15f80dafeca1
emmanuella <emmavduru@gmail.com>
2022-05-29 14:00:47 +0100

    Created home page

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+
+app.use(express.json())
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.get("/signup", (req, res) => {
+    res.render("auth/signup")
+})
+
+app.listen(5000, (err) => {
+    if (err) throw err;
+    console.log("Server running at port 5000.")
+})
